46f71216c2055826fa9175c1c0c25e3b85e9a63e
novik-coder <hupengwei2022@163.com>
2024-11-15 17:37:01 +0300

    Update Lab1Application.java

--- src/main/java/com/example/lab1/Lab1Application.java
+++ src/main/java/com/example/lab1/Lab1Application.java
@@ -1,1 +1,13 @@
-sdfsf
+package com.example.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Application.class, args);
+    }
+}
+
