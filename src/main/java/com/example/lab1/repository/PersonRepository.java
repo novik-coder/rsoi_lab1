650564817b701d4470d54c65a19cb4562ddd3a41
novik-coder <hupengwei2022@163.com>
2024-11-20 14:26:57 +0300

    Delete src/main directory

--- src/main/java/com/example/lab1/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lab1.repository;
-
-import com.example.lab1.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
